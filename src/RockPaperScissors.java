b7a93799542d2cd92577f3304c3a51dfca4a41cf
Ashley Mical <smashlandi@gmail.com>
2023-02-17 14:11:18 -0600

    Added extra some extras

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,70 +1,84 @@
-import java.util.Arrays;
+import java.awt.*;
+import java.util.*;
 import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
 
 public class RockPaperScissors {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         List<String> choiceOptions = Arrays.asList("Rock", "Paper", "Scissors");
-        String userChoice;
-        int score = 0;
+        String userChoice = "";
+        int userScore = 0;
+        int computerScore = 0;
         Scanner scanner = new Scanner(System.in);
-        char cont = 'y';
+        String cont = "y";
 
-        while (cont == ('y')) {
-            System.out.print("Time to play");
-            Thread.sleep(250);
-            System.out.print(".");
-            Thread.sleep(250);
-            System.out.print(".");
-            Thread.sleep(250);
-            System.out.println(".");
-            Thread.sleep(500);
-            System.out.println("______               _        \n" +
-                    "| ___ \\             | |       \n" +
-                    "| |_/ /  ___    ___ | | __    \n" +
-                    "|    /  / _ \\  / __|| |/ /    \n" +
-                    "| |\\ \\ | (_) || (__ |   <  _  \n" +
-                    "\\_| \\_| \\___/  \\___||_|\\_\\( ) \n" +
-                    "                          |/  \n");
-            Thread.sleep(500);
-            System.out.println("______                               \n" +
-                    "| ___ \\                              \n" +
-                    "| |_/ /  __ _  _ __    ___  _ __     \n" +
-                    "|  __/  / _` || '_ \\  / _ \\| '__|    \n" +
-                    "| |    | (_| || |_) ||  __/| |    _  \n" +
-                    "\\_|     \\__,_|| .__/  \\___||_|   ( ) \n" +
-                    "              | |                |/  \n" +
-                    "              |_|                    ");
-            Thread.sleep(500);
-            System.out.println(" _____        _                              _  \n" +
-                    "/  ___|      (_)                            | | \n" +
-                    "\\ `--.   ___  _  ___  ___   ___   _ __  ___ | | \n" +
-                    " `--. \\ / __|| |/ __|/ __| / _ \\ | '__|/ __|| | \n" +
-                    "/\\__/ /| (__ | |\\__ \\\\__ \\| (_) || |   \\__ \\|_| \n" +
-                    "\\____/  \\___||_||___/|___/ \\___/ |_|   |___/(_) \n" +
-                    "                                                \n" +
-                    "                                                ");
-            Thread.sleep(500);
+        System.out.print("Time to play");
+        Thread.sleep(250);
+        System.out.print(".");
+        Thread.sleep(250);
+        System.out.print(".");
+        Thread.sleep(250);
+        System.out.println(".");
+        Thread.sleep(750);
+        System.out.println("______               _        \n" +
+                "| ___ \\             | |       \n" +
+                "| |_/ /  ___    ___ | | __    \n" +
+                "|    /  / _ \\  / __|| |/ /    \n" +
+                "| |\\ \\ | (_) || (__ |   <  _  \n" +
+                "\\_| \\_| \\___/  \\___||_|\\_\\( ) \n" +
+                "                          |/  \n");
+        Thread.sleep(500);
+        System.out.println("______                               \n" +
+                "| ___ \\                              \n" +
+                "| |_/ /  __ _  _ __    ___  _ __     \n" +
+                "|  __/  / _` || '_ \\  / _ \\| '__|    \n" +
+                "| |    | (_| || |_) ||  __/| |    _  \n" +
+                "\\_|     \\__,_|| .__/  \\___||_|   ( ) \n" +
+                "              | |                |/  \n" +
+                "              |_|                    ");
+        Thread.sleep(500);
+        System.out.println(" _____        _                              _  \n" +
+                "/  ___|      (_)                            | | \n" +
+                "\\ `--.   ___  _  ___  ___   ___   _ __  ___ | | \n" +
+                " `--. \\ / __|| |/ __|/ __| / _ \\ | '__|/ __|| | \n" +
+                "/\\__/ /| (__ | |\\__ \\\\__ \\| (_) || |   \\__ \\|_| \n" +
+                "\\____/  \\___||_||___/|___/ \\___/ |_|   |___/(_) \n" +
+                "                                                \n" +
+                "                                                ");
+        Thread.sleep(500);
 
+
+        while (cont.equals("y")) {
             while (true) {
-                System.out.println("Type in the number of your choice: (1) Rock, (2) Paper, or (3) Scissors");
-                int choiceNumber = scanner.nextInt();
-                if (choiceNumber == 1) {
-                    userChoice = "Rock";
-                    break;
-                } else if (choiceNumber == 2) {
-                    userChoice = "Paper";
-                    break;
-                } else if (choiceNumber == 3) {
-                    userChoice = "Scissors";
-                    break;
-                } else {
-                    System.out.println("Your choice must be a number.");
-
+                try {
+                    System.out.println("Type in the number of your choice: (1) Rock, (2) Paper, or (3) Scissors");
+                    int choiceNumber = scanner.nextInt();
+                    if (choiceNumber == 1) {
+                        userChoice = "Rock";
+                        break;
+                    } else if (choiceNumber == 2) {
+                        userChoice = "Paper";
+                        break;
+                    } else if (choiceNumber == 3) {
+                        userChoice = "Scissors";
+                        break;
+                    } else {
+                        Thread.sleep(500);
+                        System.out.println();
+                        System.out.println("Invalid input. Your choice must be a number 1-3.");
+                        System.out.println();
+                        Thread.sleep(500);
+                    }
+                } catch (InputMismatchException e) {
+                    Thread.sleep(500);
+                    System.out.println();
+                    System.out.println("Invalid input. Your choice must be a number 1-3.");
+                    System.out.println();
+                    scanner.nextLine();
+                    Thread.sleep(500);
                 }
             }
+
             Thread.sleep(500);
             System.out.println("You chose " + userChoice + ".");
 
@@ -76,28 +90,62 @@ public class RockPaperScissors {
 
             if (userChoice.equals(computerChoice)) {
                 Thread.sleep(750);
+                System.out.println();
                 System.out.println("IT'S A TIE!");
                 Thread.sleep(750);
-                System.out.println("Your current score is " + score);
-            } else if (userChoice.equals("Rock") && computerChoice.equals("Scissors") || userChoice.equals("Paper") && choiceOptions.equals("Rock") || userChoice.equals("Scissors") && computerChoice.equals("Paper")) {
+                System.out.println();
+                System.out.println("***CURRENT SCORE***\n" +
+                        "You: " + userScore + "\n" +
+                        "Computer: " + computerScore + "\n");
+            } else if (userChoice.equals("Rock") && computerChoice.equals("Scissors") || userChoice.equals("Paper") && computerChoice.equals("Rock") || userChoice.equals("Scissors") && computerChoice.equals("Paper")) {
                 Thread.sleep(750);
+                System.out.println();
                 System.out.println("YOU WON!");
-                score++;
+                userScore++;
                 Thread.sleep(750);
-                System.out.println("Your current score is " + score);
+                System.out.println();
+                Toolkit.getDefaultToolkit().beep();
+                System.out.println("***CURRENT SCORE***\n" +
+                        "You: " + userScore + " *\n" +
+                        "Computer: " + computerScore + "\n");
             } else {
                 Thread.sleep(750);
+                System.out.println();
                 System.out.println("THE COMPUTER WON!");
+                computerScore++;
                 Thread.sleep(750);
-                System.out.println("Your current score is " + score);
+                System.out.println();
+                System.out.println("***CURRENT SCORE***\n" +
+                        "You: " + userScore + "\n" +
+                        "Computer: " + computerScore + " *\n");
             }
 
-            Thread.sleep(750);
-            System.out.println("Would you like to play again? (Y/N)");
-            cont = scanner.next().toLowerCase().charAt(0);
+            while (true) {
+                Thread.sleep(750);
+                System.out.println("Would you like to play again? (Y/N)");
+                cont = scanner.next().toLowerCase();
 
-            if (cont == 'n'){
-                System.out.println("Thanks for playing! Goodbye!");
+                if (cont.equals("n")) {
+                    Thread.sleep(750);
+                    System.out.println();
+                    System.out.println("Thanks for playing! Goodbye!");
+                    break;
+                } else if (cont.equals("y")) {
+                    Thread.sleep(750);
+                    System.out.print("As you wish");
+                    Thread.sleep(250);
+                    System.out.print(".");
+                    Thread.sleep(250);
+                    System.out.print(".");
+                    Thread.sleep(250);
+                    System.out.println(".");
+                    Thread.sleep(750);
+                    System.out.println();
+                    break;
+                } else {
+                    Thread.sleep(500);
+                    System.out.println("You must enter 'Y' for YES or 'N' for NO. ");
+                }
             }
         }
     }
